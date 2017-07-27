92d261e83f62766702f3420a9bc1df51ed56b92c
Arbër Kuçi <a.kuci@abidat.de>
2017-07-27 18:42:41 +0200

    Push the project to the repo

---
+++ src/main/java/com/avance/test/avancepaytest/AvancePayTestApplication.java
@@ -0,0 +1,12 @@
+package com.avance.test.avancepaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvancePayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvancePayTestApplication.class, args);
+	}
+}
