bc2ebdd2c288e71dc2efa6612544f695bf5563fb
Arbër Kuçi <a.kuci@abidat.de>
2017-07-29 00:53:03 +0200

    Forgot to add @RestController annotation in a controller.

--- src/main/java/com/avance/test/avancepaytest/controller/CriteriaController.java
+++ src/main/java/com/avance/test/avancepaytest/controller/CriteriaController.java
@@ -3,6 +3,7 @@ package com.avance.test.avancepaytest.controller;
 import com.avance.test.avancepaytest.domain.Criteria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 /**
  * Created by a.kuci on 7/29/2017.
  */
+@RestController
 @RequestMapping(value = "/criterias")
 public class CriteriaController {
 
